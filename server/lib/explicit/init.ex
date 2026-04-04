b1015cc60385ac4a16209b930c225881fbb1b5d9
Onni Hakala <onni@flaky.build>
2026-04-04 09:49:09 +0300

    Use beamMinimal28Packages.elixir_1_20 (no override needed)

--- server/lib/explicit/init.ex
+++ server/lib/explicit/init.ex
@@ -618,27 +618,10 @@ defmodule Explicit.Init do
     """
     { pkgs, lib, config, inputs, ... }:
 
-    let
-      elixir_1_20_rc4 = pkgs.beam28Packages.elixir_1_20.overrideAttrs (old: rec {
-        version = "1.20.0-rc.4";
-        src = pkgs.fetchFromGitHub {
-          owner = "elixir-lang";
-          repo = "elixir";
-          rev = "v${version}";
-          hash = "sha256-sboB+GW3T+t9gEcOGtd6NllmIlyWio1+cgWyyxE+484=";
-        };
-        doCheck = false;
-      });
-    in
     {
       languages.elixir = {
         enable = true;
-        package = elixir_1_20_rc4;
-      };
-
-      languages.erlang = {
-        enable = true;
-        package = pkgs.beam.interpreters.erlang_28;
+        package = pkgs.beamMinimal28Packages.elixir_1_20;
       };
 
       services.postgres = {
